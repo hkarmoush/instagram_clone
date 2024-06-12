e81494b048641951d284c5575b80de92ca9b895f
Hasan Armoush <hkarmoush@gmail.com>
2024-06-13 00:58:25 +0300

    AWS Integration

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "instagramclone": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
